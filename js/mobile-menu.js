87050e8165bc057964a93c14cf9821cf831d75fe
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2023-07-27 22:22:35 +0200

    menu-mob

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[menu-open]"),
+        closeModalBtn: document.querySelector("[menu-close]"),
+        modal: document.querySelector("[menu-container]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
